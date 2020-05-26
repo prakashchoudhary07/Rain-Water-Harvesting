f69f88b6961ee3f4c2c8fffe980ceebaa770bf19
Prakash Choudhary <om.pcprakash@gmail.com>
2020-05-26 21:06:56 +0530

    Solution Changed

--- rainWaterHarvesting.js
+++ rainWaterHarvesting.js
@@ -3,51 +3,35 @@
 class RainWaterHarvesting {
 
  calculateCollectedWater(arr) {
-  if (!arr) return;
-  let result = 0;
-  let rmax = false;
-  let flag = false;
-  let left = 0, right = arr.length;
+  const leftMax = [], rightMax = [];
+  const arrayLength = arr.length;
+  let max = 0;
+  let area = 0;
+  arr.forEach((element, index) => {
 
-  while (left < right) {
+   max = 0;
 
-   for (let j = left; j < arr.length; j++) {
-    if (arr[left] < arr[j]) {
-     result = result + (arr[left]) * (j - left);
-     left = j - 1;
-     rmax = true;
-     flag = true;
-     break;
-    }
-    rmax = false;
+   for (let i = index; i < arrayLength; i++) {
+    max = Math.max(max, arr[i]);
    }
+   leftMax.push(max);
 
-   if (!rmax) {
-    for (let j = right - 1; j >= left; j--) {
-     for (let k = j; k >= left; k--) {
-      if (arr[k] > arr[j]) {
-       result += (j - k) * arr[j];
-       j = k + 1;
-       right = j;
-       break
-      }
-     }
-     right--;
-    }
-   }
+   max = 0;
 
-   if (!flag) {
-    if (arr[0] === arr[arr.length - 1]) {
-     result = result + (arr[left] * arr.length - 1);
-     left = arr.length
-    }
+   for (let i = index - 1; i > -1; i--) {
+    max = Math.max(max, arr[i]);
    }
-   left++
+   rightMax.push(max);
+
+  });
+
+  for (let i = 0; i < arrayLength; i++) {
+   area += Math.min(leftMax[i], rightMax[i]);
   }
-
-  return result;
+  return area;
 
  }
+
 }
 
 module.exports = RainWaterHarvesting;
--- test.js
+++ test.js
@@ -7,5 +7,5 @@ console.log(wh.calculateCollectedWater([10, 2, 1, 16, 5, 7, 2, 11]) === 74);
 console.log(wh.calculateCollectedWater([1, 1000, 2000, 1]) === 1002);
 console.log(wh.calculateCollectedWater([1]) === 0);
 console.log(wh.calculateCollectedWater([1, 1]) === 1);
-console.log(wh.calculateCollectedWater([1, 1, 1]) === 2);
 console.log(wh.calculateCollectedWater([1, 2, 1]) === 2);
+console.log(wh.calculateCollectedWater([1, 3, 1, 3, 1]) === 8);
